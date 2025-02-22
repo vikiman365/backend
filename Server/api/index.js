f3e1c11dd568a04917574c5ca87c702d51888143
vikiman <vikiman365@gmail.com>
2025-02-22 15:09:21 +0300

    new deployment

--- Server/api/index.js
+++ Server/api/index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
 require('dotenv').config();
-
+const app = express();
+const PORT = process.env.PORT || 5000;
 const cors = require('cors');
 app.use(cors()); // This enables CORS for all routes
 
